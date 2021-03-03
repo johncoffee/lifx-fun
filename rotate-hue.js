42a2988ad44bebc444d6405e78e1101a8bdd753f
johncoffee <github2020@studio42.dk>
2021-03-03 23:18:29 +0100

    first

--- rotate-hue.js
+++ rotate-hue.js
@@ -1,23 +1,26 @@
 // Create a LifxLan object
-const Lifx  = require('node-lifx-lan');
+const Lifx = require('node-lifx-lan')
 
 rotateHue()
 
-async function rotateHue() {
+async function rotateHue () {
   let incr = 0.00025
+  let delay = 20
   let h = Math.random()
-  while(h < 1) {
+
+  while (h < 1) {
     await Lifx.setColorBroadcast({
-      color: {
-        hue: h,
-        saturation: .8,
-        brightness: .85,
-      }
+      color:
+        {
+          hue: h,
+          saturation: 1,
+          brightness: 0.3,
+        }
     })
-    await new Promise(res => setTimeout(res, 20))
+    await new Promise(res => setTimeout(res, delay))
     h += incr
 
-    if (h >= 1) {
+    if (h > 1) {
       h = 0
     }
   }
